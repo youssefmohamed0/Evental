d6e49fbc324fb3597b33a49e7899947d5dfeec6a
youssef <youssefyoussef.m4@gmail.com>
2026-09-08 00:36:09 +0300

    Inital commit

---
+++ src/main/java/joe/app/EventReservationApp/EventReservationAppApplication.java
@@ -0,0 +1,13 @@
+package joe.app.EventReservationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventReservationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventReservationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/joe/app/EventReservationApp/EventReservationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package joe.app.EventReservationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventReservationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
